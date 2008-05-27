0dd29e55ed9bac9e11d478067e58b8cf8ae34f3e
robnyman <robnyman@03c7d74f-2c4e-0410-a00a-7f0e63306f09>
2008-05-27 01:39:54 +0000

    First version

---
+++ trunk/js/facedesk.js
@@ -0,0 +1,20 @@
+/*extern air, $, $$ */
+var facedesk = function () {
+	var facebook = null;
+	var facebookFrame = null;
+	var fbNavigator = null;
+	return {
+		init : function () {
+			facebook = window.frames["facebook-content-frame"];
+			facebookFrame = $$("facebook-content");
+			if (facebook && facebookFrame) {
+				facebookFrame.onload = this.initFacebook;
+			}
+		},
+		
+		initFacebook : function () {
+			$("#theme-chooser").setStyle("display", "block");
+		}
+	};
+}();
+DOMAssistant.DOMReady("facedesk.init()");
